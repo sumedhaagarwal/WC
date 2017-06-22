478ff1f2f59b1dd56d312a9dc1d5ff0067e956bb
Sumedha Agarwal <sumedha211196@gmail.com>
2017-06-22 23:16:01 +0530

    Update youtubedownloader.py

--- youtubedownloader.py
+++ youtubedownloader.py
@@ -1,9 +1,26 @@
 import os
-a=input()
-b=input("Enter 1 for mp3 or 2 for mp4 ")
-if b=='2':
-      os.chdir('/home/sagarwal/Videos')
-      os.system('youtube-dl '+a)
+a=input('Enter URL: ')
+b=input("For audio press 1 and for video press 2: ")
+
+M_path = /path/to/music
+V_path = /path/to/video
+
+if b=='1':
+	c=input("For default path press 1 and for any other path press 2: ")
+	if c==1:
+		os.chdir(M_path)
+		os.system('youtube-dl --extract-audio --audio-format mp3 '+a)
+	else:
+		path=input("Enter Path: ")
+		os.chdir(path)
+		os.system('youtube-dl --extract-audio --audio-format mp3 '+a)
 else:
-      os.chdir('/home/sagarwal/Music')
-      os.system('youtube-dl --extract-audio --audio-format mp3 '+a)
+	c=input("For default path press 1 and for any other path press 2: ")
+	if c==1:
+		os.chdir(V_path)
+		os.system('youtube-dl '+a)
+	else:
+		path=input("Enter Path: ")
+		os.chdir(path)
+		os.system('youtube-dl '+a)
+
